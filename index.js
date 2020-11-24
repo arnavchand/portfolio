4989f859a7f9570c9286ae778589fea65a2c271c
arnavchand <43096316+arnavchand@users.noreply.github.com>
2020-11-25 01:22:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.marquee').marquee({
+      start: 100,
+         duration: 9000,
+         duplicated: true,
+         gap: 10,
+         direction: 'left',
+         pauseOnHover: false,
+    });
+});
