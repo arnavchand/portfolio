f185e27eb7ce0920be0a3fe2ce6803bde71cc672
arnavchand <43096316+arnavchand@users.noreply.github.com>
2020-12-02 21:38:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ $(function () {
          pauseOnHover: false,
     });
 });
+
+// for (var i = 0, i < document.querySelectorAll(".project-img-home").length) {
+//   document.querySelectorAll(".project-img-home")[i].addEventListener("click", function () {
+//       console.log("I got clicked!");
+//   });
+// };
+
+ 
+// hr {
+//   border-bottom: 2px solid black;
+//   height: 0.1px solid #F9F8F4;
+//   margin-bottom: 50px;
+// }
+//
+// h2 {
+//   font-family: Space Mono;
+//   font-size: 3.75rem;
+//   /*60px*/
+//   line-height: 166%;
+//   margin: 70px 0 70px 0;
+// }
+//
+// h3 {
+//   font-family: Space Mono, monospace;
+//   font-size: 1.875rem;
+//   /*30px*/
+//   line-height: 133%;
+// }
