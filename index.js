46d1eccea277404cead41eaa8fe4038064e858ea
arnavchand <43096316+arnavchand@users.noreply.github.com>
2020-12-25 02:29:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ $(function () {
     });
 });
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 // for (var i = 0, i < document.querySelectorAll(".project-img-home").length) {
 //   document.querySelectorAll(".project-img-home")[i].addEventListener("click", function () {
 //       console.log("I got clicked!");
