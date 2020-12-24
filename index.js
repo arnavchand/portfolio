e95c61615f1b29b49a66baba2f57e17f04bcd826
arnavchand <43096316+arnavchand@users.noreply.github.com>
2020-12-25 02:42:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ $(function () {
 });
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+//
+// window.addEventListener('resize', () => {
+//   // We execute the same script as before
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
 
 // for (var i = 0, i < document.querySelectorAll(".project-img-home").length) {
 //   document.querySelectorAll(".project-img-home")[i].addEventListener("click", function () {
