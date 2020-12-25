e3a43b33e68a75f35588a1c6cca4beaab0a0fe95
arnavchand <43096316+arnavchand@users.noreply.github.com>
2020-12-26 00:59:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-$(function(){$(".marquee").marquee({start:100,duration:9e3,duplicated:!0,gap:10,direction:"left",pauseOnHover:!1})});let vh=.01*window.innerHeight;document.documentElement.style.setProperty("--vh",`${vh}px`),window.addEventListener("resize",()=>{let e=.01*window.innerHeight;document.documentElement.style.setProperty("--vh",`${e}px`)});
+$(function () {
+    $('.marquee').marquee({
+      start: 100,
+         duration: 9000,
+         duplicated: true,
+         gap: 10,
+         direction: 'left',
+         pauseOnHover: false,
+    });
+});
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+// for (var i = 0, i < document.querySelectorAll(".project-img-home").length) {
+//   document.querySelectorAll(".project-img-home")[i].addEventListener("click", function () {
+//       console.log("I got clicked!");
+//   });
+// };
+
+// document.querySelector(".button-cooking").addEventListener("mouseover", function() {
+//   document.querySelector(".button-cooking").style.backgroundColor = "#400E8F";
+//   document.querySelector(".button-cooking").innerHTML = "#FFFFFF";
+// });
+
+// var n = Math.random();
+// n = 2*n;
+// n = Math.floor(n);
+//
+// var headerImages = {"headerimg1", "headerimg2"};
+// var bgColors = {"#5857F1", "#3937F9"};
+//
+// $(".header-img").src = headerImages[n];
+
+// function setHeaderImage() {
+//   var index = Math.floor(Math.random() * 2) + 1;
+//   var headerImage = "headerimg.png";
+//   console.log(index);
+//   if(index == 1)
+//     headerImage = "headerimg1.png";
+//   if(index == 2)
+//     headerImage = "headerimg.png";
+//   document.querySelectorAll(".header-img")[0].setAttribute("src", "images\\home\\" + headerImage);
+// }
+//
+// function setHeaderImageMob() {
+//   var index2 = Math.floor(Math.random() * 2) + 1;
+//   var headerImage2 = "headerimgmob.png";
+//   console.log(index);
+//   if(index2 == 1)
+//     headerImage2 = "headerimgmob1.png";
+//   if(index2 == 2)
+//     headerImage2 = "headerimgmob2.png";
+//   document.querySelectorAll(".header-img-mob")[0].setAttribute("src", "images\\home\\" + headerImage2);
+// }
